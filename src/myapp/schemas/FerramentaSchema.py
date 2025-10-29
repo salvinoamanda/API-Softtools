e41c9b03485fa3e9fd3e6e8ef3a19f1bec905ac9
GuiBarao <guibarao04@gmail.com>
2025-10-29 16:35:57 -0400

    fix enums ferramentas

--- src/myapp/schemas/FerramentaSchema.py
+++ src/myapp/schemas/FerramentaSchema.py
@@ -1,17 +1,6 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, model_validator
 from decimal import Decimal
 from typing import Optional, Annotated
-
-'''id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-nome: Mapped[str] = mapped_column(String(100), nullable=False)
-diaria: Mapped[Decimal] = mapped_column(DECIMAL(10,2), nullable = False)
-descricao: Mapped[str] = mapped_column(Text, nullable = False)
-status: Mapped[str] = mapped_column(sqlalchemy_enum(StatusFerramenta), nullable= False, default = StatusFerramenta.DISPONIVEL)
-categoria: Mapped[str] = mapped_column(sqlalchemy_enum(CategoriaFerramenta), nullable=False)
-chave_pix: Mapped[str] = mapped_column(Text, nullable=False)
-avaliacao: Mapped[int] = mapped_column(Integer, default=0)
-quantidade_avaliacoes: Mapped[int] = mapped_column(Integer, default = 0, nullable=True)
-id_proprietario: Mapped[int] = mapped_column(ForeignKey("usuario.id"), nullable=False)'''
 
 class FerramentaSchema(BaseModel):
     id: int
@@ -26,6 +15,28 @@ class FerramentaSchema(BaseModel):
     id_proprietario: int
     quantidade_estoque: int
 
+    @model_validator(mode="after")
+    def format_enums(self):
+        
+        if str(self.categoria).isnumeric():
+            if self.status == "0":
+                self.status = 'DISPONIVEL'
+            else:
+                self.status = 'INDISPONIVEL'
+
+        if str(self.categoria).isnumeric():
+            map_categoria = {"0" : "MANUAL",
+                            "1" : "ELETRICA",
+                            "2" : "PNEUMATICA",
+                            "3" : "HIDRAULICA",
+                            "4" : "MEDICAO"
+                            }
+            
+            self.categoria = map_categoria[self.categoria]
+
+        return self
+      
+      
 
 class FerramentaPreviewSchema(BaseModel):
     id: int
